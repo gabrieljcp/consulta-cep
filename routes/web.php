79ae41fdca616c71fc628911988e15213ca914d4
gabrieljcp <gabrieljcp@outlook.com>
2024-07-19 10:38:59 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CepController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/search/local/{ceps}', [CepController::class, 'search']);
+
